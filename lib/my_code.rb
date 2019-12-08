5d04b818efa0b3a9a2504d496d70c60264727ddb
GIRIJA VISWANATHAN <girijavizhwa@gmail.com>
2019-12-08 01:44:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,7 +48,12 @@ def reduce_to_total(array,sp=0)
   new_array
 end
 
-def reduce_to_all_true()
+def reduce_to_all_true(array)
+  i = 0
+  while i < array.length do
+    if !array[i] && array[i]
+  end
+  
 end
 def reduce_to_any_true()
 end
