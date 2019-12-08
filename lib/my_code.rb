f5c1cda0faa7d14b0ffb57ccc2b5b418ea0b0ebd
GIRIJA VISWANATHAN <girijavizhwa@gmail.com>
2019-12-08 01:28:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,32 @@
 # My Code here....
+def map_to_negativize(array)
+  i = 0
+  new_array = []
+  while i < array.length do
+    element = -array[i]
+    new_array << element
+    i += 1
+  end
+  new_array
+end
+
+def map_to_no_change(array)
+i = 0
+  new_array = []
+  while i < array.length do
+    new_array << array[i]
+    i += 1
+  end
+  new_array
+end
+
+def map_to_double()
+end
+def map_to_square()
+end
+def reduce_to_total()
+end
+def reduce_to_all_true()
+end
+def reduce_to_any_true()
+end
